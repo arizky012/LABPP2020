1a4341278aa3a60620435d1eb17f28d967022d04
mifmiftaa <mkhairoh@gmail.com>
2020-10-14 21:52:20 +0700

    upload tugas

---
+++ H071201023/Praktikum1/Tugasnomor3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Tugasnomor3{
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        // ketinggian menara
+        float h = input.nextFloat();
+        // sudut elevasi ujung belakang kapal
+        float a = input.nextFloat();
+        a = (float) Math.tan(Math.toRadians(a));
+        // sudut elevasi ujung depan kapal
+        float b = input.nextFloat();
+        b = (float) Math.tan(Math.toRadians(b));
+
+        if (90 > a && a > b){
+            float x = (a * h) - (b * h);
+            System.out.printf("Panjang Kapal Adalah : %.1f m", x);
+        } else {
+            System.out.printf("Errorr..");
+        }
+
+    }
+
+}
+
+
