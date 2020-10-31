6753979f13dc3b9fd442e1eaa486f54d770aed4f
nilammgfra22 <nilammagfira22@gmail.com>
2020-10-31 20:51:49 +0800

    H071201036 Nilam Magfira Az-zahra

--- H071201036/PRAKTIKUM-3/Nomor5.java
+++ H071201036/PRAKTIKUM-3/Nomor5.java
@@ -11,6 +11,7 @@ public class Nomor5 {
                 int HH = (sec / 3600) + 6;
                 int MM = (sec % 3600) / 60;
                 int SS = (sec % 3600) % 60;
+
                 if (M >= 0 && M < 90) { // jam 06.00-11.59
                     System.out.println("Selamat Pagi");
                 } else if (M >= 90 && M < 150) {// jam 12.00-15.59
@@ -22,10 +23,13 @@ public class Nomor5 {
                 } else if (M >= 270 && M < 360) {// jam 00.00-05.59
                     System.out.println("Dini Hari");
                 }
-                System.out.printf("%02d:%02d:%02d \n", HH, MM, SS);
-            } else {
-                System.out.println("End of File");
-            }
+                if (HH >= 24) {
+                    int hour = HH - 24;
+                    System.out.printf("%02d:%02d:%02d \n", hour, MM, SS);
+                } else {
+                    System.out.printf("%02d:%02d:%02d \n", HH, MM, SS);
+                }
+            } 
         }
         input.close();
     }
