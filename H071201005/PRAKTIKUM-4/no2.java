894498f6516744b4b51480dc3383e330fe7a5cf4
ntalithap <nurtalitha.putri@yahoo.com>
2020-12-31 16:21:16 +0800

    H071201005_Talitha

--- H071201005/PRAKTIKUM-4/no2.java
+++ H071201005/PRAKTIKUM-4/no2.java
@@ -1,25 +1,36 @@
 import java.util.Scanner;
 
-public class no2 {
-    public static void main(String[] args){
+class no2 {
+    public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
-        int input = obj.nextInt();
-       int[] array = new int[input];
+        int a = obj.nextInt();
+        int b = obj.nextInt();
+        int c = obj.nextInt();
+        int sum = 0;
+        int[][] matriks_a = new int[a][b];
+        int[][] matriks_b = new int[b][c];
+        int[][] matriks_c = new int[a][c];
 
-       for (int i = 0; i < input; i++) {
-           array[i] = obj.nextInt();
-       }
-       for (int i = 0; i < array.length; i++) {
-            for (int j = i+1; j < array.length; j++) {
-               int x = 1;
-               for ( int k = 2; k <= (array[j] > array[i] ? array[i] : array[j]); k++) {
-                   if (array[j] % k == 0 && array[i] % k == 0)
-                   x = k;
-               }
-            if ( x == 1)
-            System.out.printf(" %d %d \n", array[i], array[j]);
+        for (int i = 0; i < matriks_a.length; i++) {
+            for (int j = 0; j < matriks_a[i].length; j++) {
+                matriks_a[i][j] = obj.nextInt();
             }
-       }
+        }
+        for (int i = 0; i < matriks_b.length; i++) {
+            for (int j = 0; j < matriks_b[i].length; j++) {
+                matriks_b[i][j] = obj.nextInt();
+            }
+        }
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < c; j++) {
+                for (int k = 0; k < b; k++) {
+                    sum = sum + matriks_a[i][k] * matriks_b[k][j];
+                }
+                matriks_c[i][j] = sum;
+                sum = 0;
+                System.out.print(matriks_c[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
-    
 }
